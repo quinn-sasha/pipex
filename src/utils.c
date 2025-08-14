bb351b14e3f6b2053f7fe82a5d498ed1ba1e52bf
quinn-sasha <quinnsashakenta@gmail.com>
2025-08-14 18:59:34 +0900

    Modified: handle_error: add custom error message

--- src/utils.c
+++ src/utils.c
@@ -6,13 +6,16 @@
 /*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 18:44:32 by squinn            #+#    #+#             */
-/*   Updated: 2025/08/14 18:52:32 by squinn           ###   ########.fr       */
+/*   Updated: 2025/08/14 18:57:59y squinn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
-void handle_error(void) {
-  perror("Error");
+void handle_error(char *message) {
+  if (errno < 1)
+    ft_putendl_fd(message, STDERR_FILENO);
+  else
+    perror("Error");
   exit(EXIT_FAILURE);
 }
