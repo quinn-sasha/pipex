37110ceafe0d8ed31b8e108ff11e27030fd8ea7b
quinn-sasha <quinnsashakenta@gmail.com>
2025-08-15 09:18:47 +0900

    Added: pipex.h

---
+++ include/pipex.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/14 18:18:42 by squinn            #+#    #+#             */
+/*   Updated: 2025/08/15 08:44:13 by squinn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include "../libft/libft.h"
+
+# define CHILD 0
+# define READ 0
+# define WRITE 1
+# define MINIMUM_ARGS 5
+
+// Error messages
+# define USAGE "Error: ./pipex input_file cmd1 cmd2 output_file"
+
+void handle_error(char *message);
+
+
+#endif
