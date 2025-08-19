3081fa1b92b807dd05a4e3f9c2d312ebe19bb41f
quinn-sasha <quinnsashakenta@gmail.com>
2025-08-19 13:16:33 +0900

    Added: open_output_file()

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 18:18:42 by squinn            #+#    #+#             */
-/*   Updated: 2025/08/19 09:38:57 by squinn           ###   ########.fr       */
+/*   Updated: 2025/08/19 13:11:27 by squinn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,7 @@ void handle_error_and_free(char *message, int is_customized, int *pids, int exit
 t_program_args new_program_args(int argc, char **argv, char **environ, int is_heredoc);
 int is_same_string(char *s1, char *s2);
 int open_output_file(char *filename, int is_heredoc);
+int open_input_file(char *filename);
 // main.c
 char *find_path(char *command, char *environ[]);
 void execute(char *command_and_args, char *environ[], pid_t *pids);
